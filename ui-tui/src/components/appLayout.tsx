3c27b7a9d7aca6b94c51337397f27f8553f3ff57
ethernet <arilotter@gmail.com>
2026-04-30 13:49:38 -0400

    feat(tui): add ability to stash and pop prompts
    with ctrl-s / ctrl-p, letting you interrupt your own prompt to do
    something else & bring it back later

--- ui-tui/src/components/appLayout.tsx
+++ ui-tui/src/components/appLayout.tsx
@@ -24,6 +24,7 @@ import { Banner, Panel, SessionPanel } from './branding.js'
 import { FpsOverlay } from './fpsOverlay.js'
 import { MessageLine } from './messageLine.js'
 import { QueuedMessages } from './queuedMessages.js'
+import { StashIndicator } from './stashIndicator.js'
 import { LiveTodoPanel, StreamingAssistant } from './streamingAssistant.js'
 import { TextInput, type TextInputMouseApi } from './textInput.js'
 
@@ -213,6 +214,8 @@ const ComposerPane = memo(function ComposerPane({
         queueEditIdx={composer.queueEditIdx}
         t={ui.theme}
       />
+
+      <StashIndicator count={composer.stashCount} t={ui.theme} textInPrompt={!!composer.input.length || !!composer.inputBuf.length}/>
 
       {ui.bgTasks.size > 0 && (
         <Text color={ui.theme.color.muted}>
